03f3fdeebbfb98bd77036f9314946f13d911c2dd
BimaliWickramasinghe <bimalicw@gmail.com>
2021-07-18 20:27:29 +0530

    Add new features

--- src/components/FlexboxPage.js
+++ src/components/FlexboxPage.js
@@ -1,39 +1,22 @@
 import React from "react";
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Nav from 'react-bootstrap/Nav';
 import './FlexboxPage.css';
-import { Switch, Route, Link } from 'react-router-dom';
-import Body from "./body";
 
 const FlexboxPage = () => {
     return (
       <div>
-        <div class="container3" style={{marginTop:'20px', paddingBottom: '20px', color:'black'}}>
+        <div class="container3" style={{marginTop:'50px', paddingBottom: '20px', color:'black'}}>
           <div class="d-flex">
             <div class="mr-auto p-2">
-            <Nav.Item style={{color:'black'}}><p id="myName">BIMALI WICKRAMASINGHE<br/>
+            <Nav.Item style={{color:'black'}}><p id="myName">Bimali Wickramasinghe<br/>
                 <span style={{color:'grey', fontSize:'12px'}}>Software Engineering Undergraduate</span>
                 </p>
               </Nav.Item>
             </div>
-            {/* <div class="p-2">
-            <Nav.Item>
-                <Nav.Link as={Link} to="/work" style={{color:'black'}}>Work</Nav.Link>
-              </Nav.Item>
-            </div>
-            <div class="p-2">
-            <Nav.Item>
-                <Nav.Link as={Link} to="/ill" style={{color:'black'}}>Illustrations</Nav.Link>
-              </Nav.Item>
-            </div> */}
           </div>
-
           <div>
         </div>
-
-        </div>
-    
-        
+        </div> 
       </div>
     );
 }
--- src/components/body.js
+++ src/components/body.js
@@ -14,7 +14,6 @@ import HappySmileLogo from "../assets/HappySmileLogo.jpg";
 import './FlexboxPage.css';
 import { SocialIcon } from 'react-social-icons';
 
-
 console.log(image);
 
 const Body = () => {
@@ -49,55 +48,52 @@ const Body = () => {
           <h3 path='work' style={{paddingBottom:'20px'}}>Work</h3>
           <div class="row1" id="addShadow" style={{padding:'40px', marginTop:'20px'}} >
             <div class="column2">
-            <h4>'Happy Smile' Blog Website</h4>
+              <h4>'Happy Smile' Blog Website</h4>
               <img class="zoom" src={HappySmileLogo}/>
             </div>
             <div class="column2" style={{paddingTop:'30px', paddingLeft:'40px'}}>
-            <h6 style={{fontWeight: 'bold'}}>Technologies</h6>
-          <p>React, Bootstrap, JSX</p>
-          <h6 style={{fontWeight: 'bold'}}>Overview</h6>
-          <p>'Happy Smile' is a psychological blog website 
-          that helps people to develop their mental health</p>  
+              <h6 style={{fontWeight: 'bold'}}>Technologies</h6>
+              <p>React, Bootstrap, JSX</p>
+              <h6 style={{fontWeight: 'bold'}}>Overview</h6>
+              <p>'Happy Smile' is a psychological blog website 
+              that helps people to develop their mental health</p>  
             </div>
           </div>
 
           <div class="row1" id="addShadow" style={{padding:'40px', marginTop:'20px'}} >
             <div class="column2">
-            <h4>Tourism Website</h4>
+              <h4>Tourism Website</h4>
               <img class="zoom" src={tourism}/>
             </div>
             <div class="column2" style={{paddingTop:'30px', paddingLeft:'40px'}}>
-            <h6 style={{fontWeight: 'bold'}}>Technologies</h6>
-          <p>HTML, CSS, JS</p>
-          <h6 style={{fontWeight: 'bold'}}>Overview</h6>
-          <p>Tourism website is a website made from scratch 
-          as a project for 'Web Application Development' 
-          course in the university</p>  
+              <h6 style={{fontWeight: 'bold'}}>Technologies</h6>
+              <p>HTML, CSS, JS</p>
+              <h6 style={{fontWeight: 'bold'}}>Overview</h6>
+              <p>Tourism website is a website made from scratch 
+              as a project for 'Web Application Development' 
+              course in the university</p>  
             </div>
           </div>
 
           <div class="row1" id="addShadow" style={{padding:'40px', marginTop:'20px'}} >
             <div class="column2">
-            <h4>University Information System</h4>
+              <h4>University Information System</h4>
               <img class="zoom" src={code}/>
             </div>
             <div class="column2" style={{paddingTop:'30px', paddingLeft:'40px'}}>
-            <h6 style={{fontWeight: 'bold'}}>Technologies</h6>
-          <p>Java, MySQL</p>
-          <h6 style={{fontWeight: 'bold'}}>Overview</h6>
-          <p>University management systemwas done as a university 
-          as a project for 'Object Oriented Programing' 
-          course in the university. It manages student, lecturers ans subject data</p>
-          <a href={'https://github.com/BimaliWickramasinghe/University_Management_System'}>Visit Reposirory</a>  
+              <h6 style={{fontWeight: 'bold'}}>Technologies</h6>
+              <p>Java, MySQL</p>
+              <h6 style={{fontWeight: 'bold'}}>Overview</h6>
+              <p>University management systemwas done as a university 
+              as a project for 'Object Oriented Programing' 
+              course in the university. It manages student, lecturers ans subject data</p>
+              <a href={'https://github.com/BimaliWickramasinghe/University_Management_System'} target="_blank">Visit Reposirory</a>  
             </div>
           </div>
-        </div>
-
-        
+        </div>     
 
         <div class="container2-csm" style={{paddingTop:'80px'}}>
           <h3 path='ill' path="/ill">Illustrations <a href="https://www.pinterest.com/Bwicky_/poems/" target="_blank" style={{fontSize:'20px'}}>_bwicky</a></h3>
-
           <div class="row1" style={{paddingTop:'20px'}}>
             <div class="column">
               <img class="zoom" src={AVACADO}/>
@@ -105,27 +101,23 @@ const Body = () => {
               <img class="zoom"src={CUP2}/>
             </div>
             <div class="column">
-            <img class="zoom" src={COVOID}/>
-            <img class="zoom" src={MOM6}/>
-              <img class="zoom" src={SLEEPY}/>
-              
+              <img class="zoom" src={COVOID}/>
+              <img class="zoom" src={MOM6}/>
+              <img class="zoom" src={SLEEPY}/>   
             </div>
             <div class="column">
-            
-            <img class="zoom" src={COMIC}/>
-            
-            <img class="zoom" src={SANTA}/>
+              <img class="zoom" src={COMIC}/>
+              <img class="zoom" src={SANTA}/>
             </div>
           </div>
         </div>
         
         <div class="footer">
           <h4>Connect With Me!</h4><br/>
-        <SocialIcon url="https://github.com/BimaliWickramasinghe" style={{margin:'10px'}}/>
-        <SocialIcon url="https://bimalics.medium.com" style={{margin:'10px'}}/>
-        <SocialIcon url="https://linkedin.com/in/bimali-wickramasinghe-3a5984181" style={{margin:'10px'}}/>
-</div>
-
+          <SocialIcon url="https://github.com/BimaliWickramasinghe" target="_blank" style={{margin:'10px'}}/>
+          <SocialIcon url="https://bimalics.medium.com" target="_blank" style={{margin:'10px'}}/>
+          <SocialIcon url="https://linkedin.com/in/bimali-wickramasinghe-3a5984181" target="_blank" style={{margin:'10px'}}/>
+        </div>
       </div>
     );
 }
